3b55d84a28029944822a5157a43d6d872d2dc420
Astomaniano <alonso.astomaniano@gmail.com>
2024-11-17 22:19:32 +0400

    table verbouse name changes

--- manage_orders/apps.py
+++ manage_orders/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ManageOrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'manage_orders'
+    verbose_name = 'Управление заказами'
